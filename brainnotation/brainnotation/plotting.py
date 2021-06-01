7a9ca3cfc05f5a1cdaaca88a4b0a98eedb33fd9d
Ross Markello <rossmarkello@gmail.com>
2021-06-01 11:21:57 -0400

    [REF] More dependencies removed

--- brainnotation/brainnotation/plotting.py
+++ brainnotation/brainnotation/plotting.py
@@ -7,88 +7,24 @@ from pathlib import Path
 from brainnotation.images import load_gifti
 from pkg_resources import resource_filename
 
-import matplotlib.pyplot as plt
+from matplotlib import colors as mcolors, pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  # noqa
 from nilearn.plotting import plot_surf
 import numpy as np
-import seaborn as sns
 
 from brainnotation.transforms import DENSITIES, _check_hemi
 
-CIVETDIR = Path('/home/rmarkello/data/civet')
 ATLASDIR = Path(resource_filename('brainnotation', 'data/atlases'))
 HEMI = dict(L='left', R='right')
 ALIAS = dict(
     fslr='fsLR', fsavg='fsaverage', mni152='MNI152', mni='MNI152'
 )
-plt.cm.register_cmap('caret_blueorange', sns.blend_palette([
+plt.cm.register_cmap(
+    'caret_blueorange', mcolors.LinearSegmentedColormap.from_list('blend', [
         '#00d2ff', '#009eff', '#006cfe', '#0043fe',
         '#fd4604', '#fe6b01', '#ffd100', '#ffff04'
-    ], as_cmap=True)
+    ])
 )
-
-
-def plot_fslr_sulc():
-    """
-    Plots fs_LR reference sulcal depth maps
-
-    Returns
-    -------
-    fig : matplotlib.Figure
-        Plotted figure
-    """
-
-    fig, axes = plt.subplots(1, 2, subplot_kw={'projection': '3d'})
-
-    tmpdir = ATLASDIR / 'fsLR'
-    fmt = 'tpl-fsLR_den-164k_hemi-{{hemi}}{{desc}}_sphere.surf.gii'
-
-    for ax, hemi in zip(axes, ('left', 'right')):
-        hm = hemi[0].upper()
-        plot_surf(tmpdir / fmt.format(hemi=hm, desc=''),
-                  tmpdir / fmt.format(hemi=hm, desc='_desc-sulc'),
-                  hemi=hemi, axes=ax)
-    fig.tight_layout()
-
-    return fig
-
-
-def plot_civet_msm(subjdir, rot=True):
-    """
-    Plots original + rotated CIVET spheres w/sulcal depth information
-
-    Parameters
-    ----------
-    subjdir : str or os.PathLike
-        Path to data directory where `msmrot` and `msmsulc` directories live
-    rot : bool, optional
-        Whether to plot outputs from `msmrot` (True) or `msmsulc` (False).
-        Default: True
-
-    Returns
-    -------
-    fig : matplotlib.Figure
-        Plotted figure
-    """
-
-    subjdir = Path(subjdir)
-    sub = subjdir.name
-    fig, axes = plt.subplots(2, 2, subplot_kw={'projection': '3d'})
-    for n, row in enumerate(axes):
-        for ax, hemi in zip(row, ('left', 'right')):
-            if n:
-                msmdir = 'msmrot' if rot else 'msmsulc'
-                sphere = (subjdir / msmdir
-                          / f'{hemi[0].upper()}.sphere.reg.surf.gii')
-            else:
-                sphere = (CIVETDIR / sub / 'gifti'
-                          / f'{sub}_sphere_surface_rsl_{hemi}_81920.surf.gii')
-            sulc = f'{sub}_sulc_surface_rsl_{hemi}_81920.shape.gii'
-            ax = plot_surf(str(sphere), str(CIVETDIR / sub / 'gifti' / sulc),
-                           hemi=hemi, axes=ax)
-    fig.tight_layout()
-
-    return fig
 
 
 def plot_to_template(data, template, density, surf='inflated', space=None,
