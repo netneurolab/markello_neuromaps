17252c29e94c2410e904fe36b0b4ff0b9dcd6846
Ross Markello <rossmarkello@gmail.com>
2021-06-10 10:09:10 -0400

    [DOC] More doc-strings!

--- brainnotation/brainnotation/datasets/atlases.py
+++ brainnotation/brainnotation/datasets/atlases.py
@@ -24,7 +24,37 @@ DENSITIES = dict(
 )
 
 
+_atlas_docs = dict(
+    url="""\
+url : str, optional
+    URL from which to download data. Default: None\
+""",
+    data_dir="""\
+data_dir : str, optional
+    Path to use as data directory. If not specified, will check for
+    environmental variable 'BRAINNOTATION_DATA'; if that is not set, will
+    use `~/brainnotation-data` instead. Default: None\
+""",
+    verbose="""\
+verbose : int, optional
+    Modifies verbosity of download, where higher numbers mean more updates.
+    Default: 1\
+""",
+    genericatlas="""\
+atlas : dict
+    Dictionary where keys are atlas types and values are atlas files\
+""",
+    surfatlas="""\
+atlas : dict
+    Dictionary where keys are atlas types and values are tuples of atlas
+    files (L/R hemisphere)\
+"""
+)
+
+
 def _sanitize_atlas(atlas):
+    """ Checks for aliases of `atlas` and confirms valid input
+    """
     atlas = ALIAS.get(atlas, atlas)
     if atlas not in DENSITIES:
         raise ValueError(f'Invalid atlas: {atlas}.')
@@ -32,6 +62,8 @@ def _sanitize_atlas(atlas):
 
 
 def _bunch_outputs(keys, values, surface=True):
+    """ Groups `values` together (L/R) if `surface` and zips with `keys`
+    """
     if surface:
         values = [SURFACE(*values[i:i + 2]) for i in range(0, len(values), 2)]
     return Bunch(**dict(zip(keys, values)))
@@ -97,11 +129,45 @@ def fetch_civet(density='41k', url=None, data_dir=None, verbose=1):
     )
 
 
+fetch_civet.__doc__ = """
+Fetches CIVET surface atlas
+
+Parameters
+----------
+density : {{'{densities}'}}, optional
+    Density of CIVET atlas to fetch. Default: '41k'
+{url}
+{data_dir}
+{verbose}
+
+Returns
+-------
+{surfatlas}
+""".format(**_atlas_docs, densities="', '".join(DENSITIES['civet']))
+
+
 def fetch_fsaverage(density='41k', url=None, data_dir=None, verbose=1):
     keys = ['white', 'pial', 'inflated', 'sphere']
     return _fetch_atlas(
         'fsaverage', density, keys, url=url, data_dir=data_dir, verbose=verbose
     )
+
+
+fetch_fsaverage.__doc__ = """
+Fetches fsaverage surface atlas
+
+Parameters
+----------
+density : {{'{densities}'}}, optional
+    Density of fsaverage atlas to fetch. Default: '41k'
+{url}
+{data_dir}
+{verbose}
+
+Returns
+-------
+{surfatlas}
+""".format(**_atlas_docs, densities="', '".join(DENSITIES['fsaverage']))
 
 
 def fetch_fslr(density='32k', url=None, data_dir=None, verbose=1):
@@ -113,11 +179,45 @@ def fetch_fslr(density='32k', url=None, data_dir=None, verbose=1):
     )
 
 
+fetch_fslr.__doc__ = """
+Fetches fsLR surface atlas
+
+Parameters
+----------
+density : {{'{densities}'}}, optional
+    Density of fsLR atlas to fetch. Default: '32k'
+{url}
+{data_dir}
+{verbose}
+
+Returns
+-------
+{surfatlas}
+""".format(**_atlas_docs, densities="', '".join(DENSITIES['fsLR']))
+
+
 def fetch_mni152(density='1mm', url=None, data_dir=None, verbose=1):
     keys = ['T1w', 'T2w', 'PD', 'brainmask', 'CSF', 'GM', 'WM']
     return _fetch_atlas(
         'MNI152', density, keys, url=url, data_dir=data_dir, verbose=verbose
     )
+
+
+fetch_mni152.__doc__ = """
+Fetches MNI152 atlas
+
+Parameters
+----------
+density : {{'{densities}'}}, optional
+    Resolution of MNI152 atlas to fetch. Default: '1mm'
+{url}
+{data_dir}
+{verbose}
+
+Returns
+-------
+{genericatlas}
+""".format(**_atlas_docs, densities="', '".join(DENSITIES['MNI152']))
 
 
 def fetch_regfusion(atlas, url=None, data_dir=None, verbose=1):
@@ -153,24 +253,50 @@ def fetch_regfusion(atlas, url=None, data_dir=None, verbose=1):
     return _bunch_outputs(densities, data)
 
 
+fetch_regfusion.__doc__ = """
+Fetches regfusion inputs for mapping MNI152 to specified surface `atlas`
+
+Parameters
+----------
+atlas : {{'civet', 'fsaverage', 'fsLR'}}
+    Atlas to fetch
+{url}
+{data_dir}
+{verbose}
+
+Returns
+-------
+regfusion : dict
+    Dictionary where keys are surface densities and values are regfusion inputs
+""".format(**_atlas_docs)
+
+
 def fetch_atlas(atlas, density, url=None, data_dir=None, verbose=1):
     atlas = _sanitize_atlas(atlas)
     fetcher = globals()[f'fetch_{atlas.lower()}']
     return fetcher(density, url=url, data_dir=data_dir, verbose=verbose)
 
 
-def fetch_all_atlases(data_dir=None, verbose=1):
-    """
-    Fetches (and caches) all available atlases
+fetch_atlas.__doc__ = """
+Fetches specified `atlas` and `density`
 
-    Parameters
-    ----------
-    data_dir : str, optional
-        Path to use as data directory. If not specified, will check for
-        environmental variable 'BRAINNOTATION_DATA'; if that is not set, will
-        use `~/brainnotation-data` instead. Default: None
-    verbose :
-    """
+Parameters
+----------
+atlas : {{'{atlases}'}}
+    Atlas to fetch
+density : str
+    Density (or resolution) of `atlas`. Must be valid for provided `atlas`
+{url}
+{data_dir}
+{verbose}
+
+Returns
+-------
+{genericatlas}
+""".format(**_atlas_docs, atlases="', '".join(DENSITIES.keys()))
+
+
+def fetch_all_atlases(data_dir=None, verbose=1):
     atlases = {'regfusion': {}}
     for key, resolutions in DENSITIES.items():
         atlases[key] = {}
@@ -184,32 +310,46 @@ def fetch_all_atlases(data_dir=None, verbose=1):
     return atlases
 
 
+fetch_all_atlases.__doc__ = """
+Fetches (and caches) all available atlases
+
+Parameters
+----------
+{data_dir}
+{verbose}
+
+Returns
+-------
+atlases : dict
+    Nested dictionaries containing all available atlases
+"""
+
+
 def get_atlas_dir(atlas, data_dir=None):
-    """
-    Returns filepath to specified `atlas`
-
-    Parameters
-    ----------
-    atlas : str
-        Atlas for which filepath should be returned
-    data_dir : str, optional
-        Path to use as data directory. If not specified, will check for
-        environmental variable 'BRAINNOTATION_DATA'; if that is not set, will
-        use `~/brainnotation-data` instead. Default: None
-
-    Returns
-    -------
-    atlas_dir : os.PathLike
-        Full filepath to `atlas` directory
-
-    Raises
-    ------
-    ValueError
-        If provided `atlas` is not valid
-    """
     try:
         atlas = _sanitize_atlas(atlas)
     except ValueError as err:
         if atlas != 'regfusion':
             raise err
     return Path(get_data_dir(data_dir=data_dir)) / 'atlases' / atlas
+
+
+get_atlas_dir.__doc__ = """
+Returns filepath to specified `atlas`
+
+Parameters
+----------
+atlas : str
+    Atlas for which filepath should be returned
+{data_dir}
+
+Returns
+-------
+atlas_dir : os.PathLike
+    Full filepath to `atlas` directory
+
+Raises
+------
+ValueError
+    If provided `atlas` is not valid
+""".format(**_atlas_docs)
